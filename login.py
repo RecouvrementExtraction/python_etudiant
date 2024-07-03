bb7828d64e2bdfe1f2a27cacd54187301060019f
devifg <igfdev0@gmail.com>
2024-07-03 12:29:54 +0000

    modification du mot de pass

--- login.py
+++ login.py
@@ -33,7 +33,7 @@ class Login:
         
         creer_btn = Button(login_fram, text="Créer un nouveau compte", cursor="hand2", font=("times new roman",15),bd=0, bg="cyan", fg="green").place(x=30, y=320)
         
-        oubli_btn = Button(login_fram, text="mot de passe oublié", cursor="hand2", font=("times new roman",15),bd=0, bg="cyan", fg="red").place(x=300, y=320)
+        oubli_btn = Button(login_fram, text="mot de passe oublié", cursor="hand2", font=("times new roman",15),bd=0, bg="cyan", fg="red", command=self.password_oublie_fenetre).place(x=300, y=320)
         
         connect_btn = Button(login_fram, text="Connexion", cursor="hand2", font=("times new roman",15),bd=0, bg="lightgray", fg="blue", command=self.connexion).place(x=200, y=450)
         
@@ -72,41 +72,46 @@ class Login:
                 cur = con.cursor()
                 cur.execute("SELECT * FROM compte WHERE email=%s", (self.txt_email.get(),))
                 row = cur.fetchone()
-                
-                if row is not None:
+
+                if row is None:
                     messagebox.showerror("Erreur", "Cet email n'existe pas", parent=self.root)
-                    
                 else:
                     self.root2 = Toplevel()
                     self.root2.title("Mot de passe oublié")
                     self.root2.geometry("400x400+800+500")
                     self.root2.focus_force()
                     self.root2.grab_set()
-                    
-                    title = Label(self.root2, text="Mot de passe oublié", font=("algerian",20,"bold"), bg="red", fg="black")
+
+                    title = Label(self.root2, text="Mot de passe oublié", font=("times new roman", 20, "bold"), bg="red", fg="black")
                     title.pack(side=TOP, fill=X)
-                    
-                    
-                    #Question
-                    aff_question = Label(self.root2, text="Séléctionnez une question", font=("algerian", 15, "bold"), bg="grey", fg="black").place(x=70, y=50)
-                    self.ecrit_question = ttk.Combobox(self.root2, font=("algerian", 15), state="readonly")
+
+                    # Question
+                    aff_question = Label(self.root2, text="Séléctionnez une question", font=("times new roman", 15, "bold"), bg="white", fg="black")
+                    aff_question.place(x=70, y=50)
+                    self.ecrit_question = ttk.Combobox(self.root2, font=("times new roman", 15), state="readonly")
                     self.ecrit_question["values"] = ("Select", "Ton surnom", "Lieu de naissance", "Meilleur ami", "Film préféré")
                     self.ecrit_question.place(x=70, y=100, width=250)
                     self.ecrit_question.current(0)
-                    
-                    #Reponses
-                    aff_repondre = Label(self.root2, text="Répondre", font=("algerian", 15, "bold"), bg="grey", fg="black").place(x=70, y=150)
-                    self.ecrit_repondre = Entry(self.root2,font=("times new roman",15), bg="white")
-                    self.ecrit_repondre.place(x=70, y=20, width=250)
-                    
-                    #changer mot de passe
-                    aff_nouvpass = Label(self.root2, text="Nouveau mot de passe", font=("algerian", 15, "bold"), bg="grey", fg="black").place(x=70, y=150)
-                    self.ecrit_nouvpass = Entry(self.root2,font=("times new roman",15), bg="white")
-                    self.ecrit_nouvpass.place(x=70, y=20, width=250)
-                    
-            
+
+                    # Réponses
+                    aff_repondre = Label(self.root2, text="Répondre", font=("times new roman", 15, "bold"), bg="white", fg="black")
+                    aff_repondre.place(x=70, y=150)
+                    self.ecrit_repondre = Entry(self.root2, font=("times new roman", 15), bg="white")
+                    self.ecrit_repondre.place(x=70, y=200, width=250)
+
+                    # Changer mot de passe
+                    aff_nouvpass = Label(self.root2, text="Nouveau mot de passe", font=("times new roman", 15, "bold"), bg="white", fg="black")
+                    aff_nouvpass.place(x=70, y=250)
+                    self.ecrit_nouvpass = Entry(self.root2,show="*", font=("times new roman", 15), bg="white")
+                    self.ecrit_nouvpass.place(x=70, y=300, width=250)
+
+                    # Bouton changer mot de passe
+                    changer_btn = Button(self.root2, text="Modifier", cursor="hand2", font=("times new roman", 15, "bold"), bg="lightgray", fg="green", command=self.passwd_oublie)
+                    changer_btn.place(x=160, y=350)
+
             except Exception as ex:
                 messagebox.showerror("Erreur", f"Erreur de connexion : {str(ex)}", parent=self.root)
+
         
             finally:
                 if con:
@@ -116,7 +121,32 @@ class Login:
     
 
 
-
+    def passwd_oublie(self):
+        if self.ecrit_question.get()=="" or self.ecrit_repondre.get()=="" or self.ecrit_nouvpass.get()=="":
+            messagebox.showerror("Erreur","Remplir tous les champs", parent=self.root2)
+        else:
+            try:
+                con = pymysql.connect(host="localhost", user="root", passwd="", database="python_etudiant")
+                cur = con.cursor()
+                cur.execute("SELECT * FROM compte WHERE email=%s and question=%s and reponse=%s", (self.txt_email.get(), self.ecrit_question.get(), self.ecrit_repondre.get()))
+                row = cur.fetchone()
+                
+                if row is None:
+                    messagebox.showerror("Erreur", "vous n'avez pas reondu a la question séléctionnée", parent=self.root2)
+                else:
+                     cur.execute("UPDATE compte set  password=%s where email=%s", (self.ecrit_nouvpass.get(),self.txt_email.get(),))
+                     con.commit()
+                     messagebox.showinfo("Succès", "mot de passe modifié", parent=self.root2)
+                
+            except Exception as es:
+                 messagebox.showerror("Erreur", f"Erreur de connexion : {str(es)}", parent=self.root)
+                 
+            finally:
+                if con:
+                    con.close()
+                 
+                 
+            
 
 
 
